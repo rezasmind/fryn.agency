019cc778c53e4d88385233aa3440ae99bc5c236f
Reza Aghajani <rezaaghajani@protonmail.com>
2023-06-23 20:32:58 +0330

    Fix Website

--- fryn.agency/src/components/Footer.tsx
+++ fryn.agency/src/components/Footer.tsx
@@ -11,14 +11,14 @@ const Footer = () => {
         <>
             <Image src={bar} width={2040}  alt='' className=' bg-rose-500' />
             <div className="bg-rose-500 h-auto  font-bodyFont text-white pt-40">
-                <div className="title flex flex-col lg:px-20 px-10 lg:items-normal items-center">
+                <div className="title flex flex-col lg:px-20 px-10 lg:items-end items-center">
                 <h1 className='lg:text-9xl text-6xl font-bold lg:text-right text-center'>
                 همین الان شروع کنید.
                 </h1>
                 <Link href={'/contact-us'} className='hover:bg-rose-500 hover:text-white transition-all delay-75  ease-in'><p className='py-4 px-10 mt-10 bg-white rounded-full inline-block lg:text-3xl text-xl mb-10 text-rose-500 font-bold'>تماس با ما</p></Link>
                 </div>
-                <div className="flex lg:flex-row flex-col justify-center items-center px-7"> 
-                <Image src={speaker} alt='' className=' lg:block hidden lg:w-auto w-[40vw]'/>
+                <div className="flex lg:flex-row flex-col justify-end lg:items-end items-center lg:p-0 px-7"> 
+                <Image src={speaker} alt='' className=' lg:block hidden lg:w-[60vw] w-[40vw] -translate-x-40'/>
                 <div className="call lg:px-20 pb-4 flex lg:flex-row-reverse flex-col gap-5 items-center justify-center">
                 <Link href={'https://instagram.com/fryn.agency'} className=''>
                     <BsInstagram size={24}/>
--- fryn.agency/src/pages/index.tsx
+++ fryn.agency/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import Link from "next/link";
 import Header from "~/components/Header";
-import Image from 'next/image'
-import character from '../../public/Photo.svg'
-import circle from '../../public/pattern-circle.svg'
-import speaker from '../../public/speaker-1.svg'
-import lightning from '../../public/lightning.svg'
-import portfolio from '../../public/Portfolio.svg'
+import Image from "next/image";
+import character from "../../public/Photo.svg";
+import circle from "../../public/pattern-circle.svg";
+import speaker from "../../public/speaker-1.svg";
+import lightning from "../../public/lightning.svg";
+import portfolio from "../../public/Portfolio.svg";
 import Footer from "~/components/Footer";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -18,65 +19,124 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Header />
-      <div className=" bg-rose-500 w-full lg:h-[75vh]  h-[30vh]">
-        <div className="title text-white font-bodyFont font-bold flex flex-col items-center lg:pt-44 pt-28">
-          <span className=" lg:text-9xl text-3xl">
-          خـــــلاقـــــیـــــت از مـــــــــا
-          </span>
-          <span className=" lg:text-9xl text-3xl">
-          رشـــــــد از&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;شــــــمــــــــا          </span>
-          <Image src={character} width={2040} alt="" className=" lg:-translate-y-44 -translate-y-[7vh]" />
+      <div className=" h-[30vh] w-full bg-rose-500  lg:h-[88vh]">
+        <div className="title flex flex-col items-center pt-28 font-bodyFont font-bold text-white lg:pt-44">
+          <motion.span
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className=" text-3xl lg:text-9xl"
+          >
+            خـــــلاقـــــیـــــت از مـــــــــا
+          </motion.span>
+          <motion.span
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className=" text-3xl lg:text-9xl"
+          >
+            رشـــــــد
+            از&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;شــــــمــــــــا{" "}
+          </motion.span>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+          >
+            <motion.div>
+              <Image
+                src={character}
+                width={2040}
+                alt=""
+                className=" -translate-y-[7vh] lg:-translate-y-44"
+              />
+            </motion.div>
+          </motion.div>
         </div>
       </div>
-      <div className=" bg-slate-200 h-auto flex flex-col font-bodyFont text-black px-20">
-        <div className="title lg:mt-72 mt-44 flex flex-col items-end">
-          <h1 className=" lg:text-9xl text-6xl font-bold">خدمات ما</h1>
-          <p className="text-md lg:text-right text-center max-w-lg">
-          ما یک تیم دیجیتال مارکتینگ در مازندران هستیم و تمرکز ما بر ارائه خدمات مبتنی محتوا هست و به دنبال افزایش فروش و شناخت شدن برند شما هستیم
+      <motion.div className=" flex h-auto flex-col bg-slate-200 px-20 font-bodyFont text-black">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 1.5 }}
+          className="title mt-44 flex flex-col items-end lg:mt-96"
+        >
+          <h1 className=" text-6xl font-bold lg:text-9xl">خدمات ما</h1>
+          <p className="text-md max-w-lg text-center lg:text-right">
+            ما یک تیم دیجیتال مارکتینگ در مازندران هستیم و تمرکز ما بر ارائه
+            خدمات مبتنی محتوا هست و به دنبال افزایش فروش و شناخت شدن برند شما
+            هستیم
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ ease: "easeOut", duration: 1.5 }}
+          className="services mb-56 mt-20 flex flex-col gap-12"
+        >
+          <div className="service flex h-24 cursor-pointer flex-row-reverse items-center justify-between rounded-full bg-white p-10 transition-all delay-75 ease-in hover:bg-rose-500 hover:text-white ">
+            <h1 className="text-2xl font-bold lg:text-3xl">برندینگ</h1>
+            <Image src={circle} width={50} alt="" />
+          </div>
+
+          <div className="service flex h-24 cursor-pointer flex-row-reverse items-center justify-between rounded-full bg-white p-10 transition-all delay-75 ease-in hover:bg-rose-500 hover:text-white">
+            <h1 className="text-2xl font-bold lg:text-3xl">تبلیغات</h1>
+            <Image src={circle} width={50} alt="" />
+          </div>
+
+          <div className="service flex h-24 cursor-pointer flex-row-reverse items-center justify-between rounded-full bg-white p-10 transition-all delay-75 ease-in hover:bg-rose-500 hover:text-white">
+            <h1 className="text-2xl font-bold lg:text-3xl">تولید محتوا</h1>
+            <Image src={circle} width={50} alt="" />
+          </div>
+
+          <div className="service flex h-24 cursor-pointer flex-row-reverse items-center justify-between rounded-full bg-white p-10 transition-all delay-75 ease-in hover:bg-rose-500 hover:text-white">
+            <h1 className="text-2xl font-bold lg:text-3xl">
+              اینستاگرام مارکتینگ
+            </h1>
+            <Image src={circle} width={50} alt="" />
+          </div>
+
+          <div className="service flex h-24 cursor-pointer flex-row-reverse items-center justify-between rounded-full bg-white p-10 transition-all delay-75 ease-in hover:bg-rose-500 hover:text-white">
+            <h1 className="text-2xl font-bold lg:text-3xl">طراحی سایت</h1>
+            <Image src={circle} width={50} alt="" />
+          </div>
+        </motion.div>
+      </motion.div>
+
+      <div className="third h-auto bg-rose-500 px-16 py-40 font-bodyFont text-white lg:px-52">
+        <motion.div
+          initial={{ x: 200 }}
+          whileInView={{ x: 0 }}
+          transition={{ ease: "easeIn", duration: 3 }}
+          className=""
+        >
+          <Image
+            src={speaker}
+            alt=""
+            className="translate-x-[4vw] translate-y-[4vh] lg:translate-y-[7vh]"
+          />
+          <h1 className="text-justify text-3xl font-bold leading-normal lg:text-center lg:text-8xl">
+            محتوا کلید اصلی فروش! شما با محتوای درست میتونید مخاطبان رو
+            عــــــاشـــــق خــــــــودتـــــون کـنید!
+          </h1>
+          <Image
+            src={lightning}
+            alt=""
+            className="-translate-x-[12vw] -translate-y-[8vh] lg:-translate-y-20 lg:translate-x-[10vw]"
+          />
+        </motion.div>
+      </div>
+
+      <div className="fourth flex h-auto flex-col items-center px-10 font-bodyFont text-black lg:px-20">
+        <div className="title mt-56 flex flex-col items-end">
+          <h1 className=" mb-6 text-6xl font-bold lg:text-9xl">پروژه های ما</h1>
+          <p className="text-md mb-10 max-w-xl text-center">
+            در این قسمت ما بخشی از پروژه های موفق با مشتریانمون رو با شما به
+            اشتراک میذاریم تا به راحتی بتونید از کیفیت خدمات ما اطمینان خاطر
+            کنید{" "}
           </p>
         </div>
-        <div className="services mt-20 flex flex-col gap-12 mb-56">
-          <div className="service h-24 bg-white rounded-full flex flex-row-reverse items-center p-10 justify-between hover:bg-rose-500 hover:text-white transition-all delay-75 cursor-pointer ease-in ">
-            <h1 className="font-bold lg:text-3xl text-2xl">برندینگ</h1>
-            <Image src={circle} width={50} alt="" />
-          </div>
-
-          <div className="service h-24 bg-white rounded-full flex flex-row-reverse items-center p-10 justify-between hover:bg-rose-500 hover:text-white transition-all delay-75 cursor-pointer ease-in">
-            <h1 className="font-bold lg:text-3xl text-2xl">تبلیغات</h1>
-            <Image src={circle} width={50} alt="" />
-          </div>
-
-          <div className="service h-24 bg-white rounded-full flex flex-row-reverse items-center p-10 justify-between hover:bg-rose-500 hover:text-white transition-all delay-75 cursor-pointer ease-in">
-            <h1 className="font-bold lg:text-3xl text-2xl">تولید محتوا</h1>
-            <Image src={circle} width={50} alt="" />
-          </div>
-
-          <div className="service h-24 bg-white rounded-full flex flex-row-reverse items-center p-10 justify-between hover:bg-rose-500 hover:text-white transition-all delay-75 cursor-pointer ease-in">
-            <h1 className="font-bold lg:text-3xl text-2xl">اینستاگرام مارکتینگ</h1>
-            <Image src={circle} width={50} alt="" />
-          </div>
-
-          <div className="service h-24 bg-white rounded-full flex flex-row-reverse items-center p-10 justify-between hover:bg-rose-500 hover:text-white transition-all delay-75 cursor-pointer ease-in">
-            <h1 className="font-bold lg:text-3xl text-2xl">طراحی سایت</h1>
-            <Image src={circle} width={50} alt="" />
-          </div>
-        </div>
-      </div>
-
-      <div className="third bg-rose-500 h-auto font-bodyFont text-white py-40 lg:px-52 px-16">
-        <Image src={speaker} alt="" className="lg:translate-y-[7vh] translate-y-[4vh] translate-x-[4vw]"/>
-          <h1 className="lg:text-8xl font-bold lg:text-center leading-normal text-3xl text-justify">
-          محتوا کلید اصلی فروش! شما با محتوای درست میتونید مخاطبان رو عــــــاشـــــق خــــــــودتـــــون کـنید!
-          </h1>
-          <Image src={lightning} alt="" className="lg:-translate-y-20 -translate-y-[8vh] lg:translate-x-[10vw] -translate-x-[12vw]"/>
-      </div>
-
-      <div className="fourth h-auto flex flex-col font-bodyFont text-black lg:px-20 px-10 items-center">
-      <div className="title mt-56 flex flex-col items-end">
-          <h1 className=" lg:text-9xl text-6xl font-bold mb-6">پروژه های ما</h1>
-          <p className="text-md text-center max-w-xl mb-10">در این قسمت ما بخشی از پروژه های موفق با مشتریانمون رو با شما به اشتراک میذاریم تا به راحتی بتونید از کیفیت خدمات ما اطمینان خاطر کنید          </p>
-      </div>
-      <Image src={portfolio} alt="" className="mb-56" />
+        <Image src={portfolio} alt="" className="mb-56" />
       </div>
       <Footer />
     </>
