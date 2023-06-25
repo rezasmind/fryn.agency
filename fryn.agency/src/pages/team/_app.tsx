62cfbbc86f091039c6bb221b80329dfe5569c22c
Reza Aghajani <rezaaghajani@protonmail.com>
2023-06-25 22:32:21 +0330

    fix

--- fryn.agency/src/pages/team/_app.tsx
+++ fryn.agency/src/pages/team/_app.tsx
@@ -1,6 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
-import '../styles/burger.css'
+
 
 const Team: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
