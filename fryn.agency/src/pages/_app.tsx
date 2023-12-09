2f5a8ca35e146ab85a7c0de1a3c20c58549dfdc9
Reza Aghajani <rezaaghajani@protonmail.com>
2023-12-09 15:24:58 +0330

    Analytics

--- fryn.agency/src/pages/_app.tsx
+++ fryn.agency/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/burger.css";
 
@@ -8,6 +9,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <NextUIProvider>
       <Component {...pageProps} />
+      <Analytics />
     </NextUIProvider>
   );
 };
