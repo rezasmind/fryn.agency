0630e95bfe0e27adb8e2bd05e065a8e66e0f907e
Reza Aghajani <rezaaghajani@protonmail.com>
2023-12-09 21:56:46 +0330

    Fix Calculator

--- fryn.agency/src/pages/calculator/index.tsx
+++ fryn.agency/src/pages/calculator/index.tsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import Footer from "~/components/Footer";
 import Header from "~/components/Header";
-import Head from "next/head"
+import Head from "next/head";
+import { Input } from "@nextui-org/react";
+import { Checkbox } from "@nextui-org/react";
+import { RadioGroup, Radio } from "@nextui-org/react";
+import { Button, ButtonGroup } from "@nextui-org/react";
+import { Card, CardBody } from "@nextui-org/react";
 
 const Calculator = () => {
   const [price, setPrice] = useState(0);
   const [reels, setReels] = useState(0);
   const [teaser, setTeaser] = useState(0);
   const [shots, setShots] = useState(0);
-  const [admin, setAdmin] = useState(1);
+  const [admin, setAdmin] = useState(4);
   const [marketer, setMarketer] = useState(0);
   const [discount, setDiscount] = useState(0);
-  const [phone, setPhone] = useState(0)
+  const [phone, setPhone] = useState(0);
 
   const changeReels = (e: any) => {
     setReels(e.target.value);
@@ -22,13 +27,12 @@ const Calculator = () => {
   };
 
   const changeTeaser = (e: any) => {
-    setTeaser(e.target.value)
-  }
+    setTeaser(e.target.value);
+  };
 
   const changePhone = (e: any) => {
-    setPhone(e.target.value)
-  }
-
+    setPhone(e.target.value);
+  };
 
   const calculatePrice = () => {
     var shotsPrice = shots * 450000;
@@ -39,138 +43,153 @@ const Calculator = () => {
 
     if (admin == 1) {
       adminPrice = 5500000;
-      setPrice(1000000 + shotsPrice + reelsPrice + adminPrice + teaserPrice + phonePrice);
+      setPrice(
+        1000000 +
+          shotsPrice +
+          reelsPrice +
+          adminPrice +
+          teaserPrice +
+          phonePrice
+      );
     } else if (admin == 2) {
       adminPrice = 3500000;
-      setPrice(1000000 + shotsPrice + reelsPrice + adminPrice + teaserPrice+ phonePrice);
+      setPrice(
+        1000000 +
+          shotsPrice +
+          reelsPrice +
+          adminPrice +
+          teaserPrice +
+          phonePrice
+      );
     } else if (admin == 3) {
       adminPrice = 2000000;
-      setPrice(1000000 + shotsPrice + reelsPrice + adminPrice + teaserPrice+ phonePrice);
+      setPrice(
+        1000000 +
+          shotsPrice +
+          reelsPrice +
+          adminPrice +
+          teaserPrice +
+          phonePrice
+      );
     } else if (admin == 4) {
       adminPrice = 0;
-      setPrice(1000000 + shotsPrice + reelsPrice + adminPrice + teaserPrice+ phonePrice);
+      setPrice(
+        1000000 +
+          shotsPrice +
+          reelsPrice +
+          adminPrice +
+          teaserPrice +
+          phonePrice
+      );
     }
 
-    
-
-    setDiscount(price * 0.80);
+    setDiscount(price * 0.8);
   };
 
   return (
     <>
-    <Head>
-      <title>فرم محاسبه قیمت</title>
-    </Head>
+      <Head>
+        <title>فرم محاسبه قیمت</title>
+      </Head>
       <Header />
       <div className="mb-52 flex w-full flex-col items-center justify-center gap-8 font-bodyFont text-white">
         <h1 className="mt-12 text-3xl font-bold text-white">
           فرم محاسبه قیمت خدمات فریان
         </h1>
 
-        <div className="mx-5 h-auto w-auto bg-white p-12 text-right text-blue-500 md:w-1/3">
-        <form action="" className="text-center">
-            <h1 className="mb-3 text-xl">تولید محتوا ویدیویی ( ریلز با گوشی)</h1>
-            عدد{" "}
-            <input
-              type="number"
-              name=""
-              id="reels"
-              className="  border-b-2 border-blue-500 bg-transparent"
-              onChange={changePhone}
-              value={phone}
-            />
-          </form>
-          <hr className="my-3" />
-          <form action="" className="text-center">
-            <h1 className="mb-3 text-xl">تولید محتوا ویدیویی (ریلز)</h1>
-            عدد{" "}
-            <input
-              type="number"
-              name=""
-              id="reels"
-              className="  border-b-2 border-blue-500 bg-transparent"
-              onChange={changeReels}
-              value={reels}
-            />
-          </form>
+        <div className="mx-5 flex h-auto flex-col items-center justify-center rounded-lg border-3 border-opacity-30 bg-white p-12 text-right text-blue-500 md:w-1/3">
+          <Input
+            type="number"
+            name=""
+            id="reels"
+            className=" flex justify-center bg-transparent text-right"
+            color="primary"
+            onChange={changePhone}
+            labelPlacement="inside"
+            label="ریلز با گوشی"
+            placeholder=""
+          />
           <hr className="my-3" />
 
-          <form action="" className="text-center">
-            <h1 className="mb-3 text-xl"> تیزر </h1>
-            عدد{" "}
-            <input
-              type="number"
-              name=""
-              id="reels"
-              className="  border-b-2 border-blue-500 bg-transparent"
-              onChange={changeTeaser}
-              value={teaser}
-            />
-          </form>
+          <Input
+            type="number"
+            name=""
+            color="primary"
+            id="reels"
+            className="   flex justify-center bg-transparent"
+            onChange={changeReels}
+            labelPlacement="inside"
+            label="ریلز با دوربین"
+          />
+
           <hr className="my-3" />
 
-          <form action="" className="text-center">
-            <h1 className="mb-3 text-xl">عکاسی</h1>
-            عدد{" "}
-            <input
-              type="number"
-              name=""
-              id="shot"
-              className=" border-b-2 border-blue-500 bg-transparent"
-              onChange={changeShots}
-              value={shots}
-            />
-          </form>
+          <Input
+            type="number"
+            name=""
+            color="primary"
+            labelPlacement="inside"
+            id="reels"
+            className=" flex justify-center bg-transparent"
+            onChange={changeTeaser}
+            label="تیزر"
+          />
+
+          <hr className="my-3" />
+
+          <Input
+            type="number"
+            name=""
+            color="primary"
+            labelPlacement="inside"
+            id="shot"
+            className="  flex justify-center bg-transparent"
+            onChange={changeShots}
+            label=" عکاسی"
+          />
+
           <hr className="my-3" />
           <h1 className="mb-3 text-center text-xl">ادمین استوری</h1>
           <form
             action=""
             className="flex w-full flex-row-reverse items-center justify-center gap-3"
           >
-            <label htmlFor="" className="">
-              {" "}
-              میخواهم
-            </label>
-            <input
-              type="radio"
-              name="plan"
-              id="admin-1"
-              onClick={() => setAdmin(1)}
-            />
-            <label htmlFor="" className="">
-              نمیخواهم{" "}
-            </label>
-            <input
-              type="radio"
-              name="plan"
-              id="admin-4"
-              onClick={() => setAdmin(4)}
-            />
+            <RadioGroup
+              value="4"
+              onValueChange={setAdmin}
+              orientation="horizontal"
+            >
+              <label htmlFor="" className="">
+                نمیخواهم{" "}
+              </label>
+              <Radio type="radio" name="plan" id="admin-4" value="4" />
+              <label htmlFor="" className="">
+                {" "}
+                میخواهم
+              </label>
+              <Radio
+                type="radio"
+                name="plan"
+                id="admin-1"
+                value="1"
+                defaultChecked
+              />
+            </RadioGroup>
           </form>
           <hr className="my-3" />
-          <form
-            action=""
-            className="flex w-full flex-col-reverse flex-wrap items-center justify-center gap-3"
+          <Card
+            isBlurred
+            className=" bg-blue-600 text-white shadow-md shadow-blue-500"
           >
-            <label className="">گزارش هفتگی </label>
-            <input type="checkbox" name="" id="gozaresh" checked disabled />
+            <CardBody>
+              <p className="text-center">
+                گزارش هفتگی / ارائه استراتژی محتوا و برنامه ریزی محتوایی / طراحی
+                قالب اختصاصی برای اینستاگرام / مدیریت پیج - پاسخدهی به دایرکت و
+                کامنت ها - افزایش تعامل پیج و تعامل با مخاطبین هدف{" "}
+              </p>
+            </CardBody>
+          </Card>
 
-            <label className="">
-              {" "}
-              ارائه استراتژی محتوا و برنامه ریزی محتوایی{" "}
-            </label>
-            <input type="checkbox" name="" id="" checked disabled />
-
-            <label className="">طراحی قالب اختصاصی برای اینستاگرام </label>
-            <input type="checkbox" name="" id="" checked disabled />
-
-            <label className="text-center">
-              {" "}
-              مدیریت پیج - پاسخدهی به دایرکت و کامنت ها - افزایش تعامل پیج و
-              تعامل با مخاطبین هدف{" "}
-            </label>
-            <input type="checkbox" name="" id="" checked disabled />
-          </form>
           <hr className="my-3" />
           {/* <p className="my-4 text-center">
             هزینه ها: <br />
@@ -182,12 +201,16 @@ const Calculator = () => {
             <br />
           </p> */}
           <div className="flex flex-col items-center justify-center">
-            <button
-              className="border-1 mb-3 rounded-sm border border-blue-500 px-4 py-2 hover:bg-blue-500 hover:text-white"
+            <Button
+              className="mb-3"
               onClick={calculatePrice}
+              color="primary"
+              size="lg"
+              radius="sm"
+              variant="shadow"
             >
               محاسبه قیمت
-            </button>
+            </Button>
 
             <h1 className="font-bold">
               قیمت نهایی: {price.toLocaleString()} تومان
